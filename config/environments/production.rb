a88511778aada31066fd9458b374c54b2721eae1
Lionel Jingles <mrjingles@gmail.com>
2014-05-28 00:15:24 -0700

    adding url to prod settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ IydWeb::Application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.url_base = "http://itsyourdistrict.org"
     
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
